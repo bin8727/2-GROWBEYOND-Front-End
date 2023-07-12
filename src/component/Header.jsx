01012e7c1f4e5c2878e740e45960f23179488da1
MoYuBin <bin8727@naver.com>
2023-07-12 21:09:47 +0900

    FreeConsultation Setup

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,22 +1,92 @@
 import { Link } from "react-router-dom";
 import Logo from '../assets/patent.png';
-
+import { useState,useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [isActived, setIsActived] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const storedIsActived = localStorage.getItem('isActived');
+    setIsActived(storedIsActived || null);
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setIsActived('header');
+    } else if (location.pathname === '/feasibility') {
+      setIsActived('feasibility');
+    } else if (location.pathname === '/patent-kr') {
+      setIsActived('patentKr');
+    } else if (location.pathname === '/patent-gb') {
+      setIsActived('patentGb');
+    } else if (location.pathname === '/costguide') {
+      setIsActived('costguide');
+    } else if (location.pathname === '/processguide') {
+      setIsActived('processguide');
+    } else if (location.pathname === '/free-consultation') {
+      setIsActived('');
+    }
+  }, [location]);
+
+  const handleClick = (e) => {
+    setIsActived(e);
+    localStorage.setItem('isActived', e);
+  };
+
   return (
     <header className='header'>
       <div className='header_wrap'>
         <div className='header_logo'>
-          <Link to='/'>
+          <Link 
+            to='/'
+            className={isActived === 'header' ? 'active' : ''}
+            onClick={() => handleClick('header')}
+          >
             <img src={Logo} alt='logo' />
           </Link>
         </div>
         <div className='header_nav'>
-          <Link to='/feasibility'>특허등록가능성검토</Link>
-          <Link to='/patent-kr'>국내특허등록신청</Link>
-          <Link to='/patent-gb'>해외특허등록신청</Link>
-          <Link to='/costguide'>비용안내</Link>
-          <Link to='/processguide'>절차안내</Link>
+          <Link 
+            to='/feasibility'
+            className={isActived === 'feasibility' ? 'active' : ''}
+            onClick={() => handleClick('feasibility')}
+          >
+            특허등록가능성검토
+          </Link>
+
+          <Link 
+            to='/patent-kr'
+            className={isActived === 'patentKr' ? 'active' : ''}
+            onClick={() => handleClick('patentKr')}
+          >
+            국내특허등록신청
+          </Link>
+
+          <Link 
+            to='/patent-gb'
+            className={isActived === 'patentGb' ? 'active' : ''}
+            onClick={() => handleClick('patentGb')}
+          >
+            해외특허등록신청
+          </Link>
+
+          <Link 
+            to='/costguide'
+            className={isActived === 'costguide' ? 'active' : ''}
+            onClick={() => handleClick('costguide')}
+          >
+            비용안내
+          </Link>
+
+          <Link 
+            to='/processguide'
+            className={isActived === 'processguide' ? 'active' : ''}
+            onClick={() => handleClick('processguide')}
+          >
+            절차안내
+          </Link>
         </div>
       </div>
     </header>
--- src/pages/FreeConsultation.jsx
+++ src/pages/FreeConsultation.jsx
@@ -1,6 +1,45 @@
+import Earth from '../assets/15.png';
+import Arrow from '../assets/arrow.svg';
+
 const FreeConsoltation = () => {
   return (
-    <></>
+    <div className="freeconsultation">
+      <div className="freeconsultation_container">
+        <div className='freeconsultation_topContent'>
+          <p className="freeconsultation_topTitle">
+            특허의 신 무료 상담소
+          </p>
+          <h2 className='freeconsultation_topDesc'>
+            무료 상담도
+            <br></br>
+            특허의 신과 함께
+          </h2>
+          <div className="freeconsultation_imgContainer">
+            <span>
+              <span></span>
+              <img src={Earth} alt="" />
+            </span>
+          </div>
+        </div>
+
+        <div className='freeconsultation_middleWrap'>
+          <h2 className='freeconsultation_middleDesc'>
+            특허 출원 전문 변리사가
+            <br></br>
+            빠르고 정확하게 상담해드립니다.
+          </h2>
+
+          <div className='freeconsultation_underArrow'>
+            <p>지금 바로 신청하기</p>
+            <button>
+              <img src={Arrow} alt="" />
+            </button>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
   );
 };
 
