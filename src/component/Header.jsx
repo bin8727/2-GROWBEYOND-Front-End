0c505ccd7367c60b5d6f5922f12b26003526e20a
MoYuBin <bin8727@naver.com>
2023-07-14 00:31:11 +0900

    헤더 수정

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -36,54 +36,64 @@ const Header = () => {
   };
 
   return (
-    <header className='header'>
-      <div className='header_wrap'>
-        <div className='header_logo'>
-          <Link 
-            to='/'
-            className={isActived === 'header' ? 'active' : ''}
-            onClick={() => handleClick('header')}
+    <header className="header">
+      <div className="header__wrap">
+        <div className="header__logo">
+          <Link
+            to="/"
+            className="header__logo--active"
+            onClick={() => handleClick("header")}
           >
-            <img src={Logo} alt='logo' />
+            <img src={Logo} alt="logo" />
           </Link>
         </div>
-        <div className='header_nav'>
-          <Link 
-            to='/feasibility'
-            className={isActived === 'feasibility' ? 'active' : ''}
-            onClick={() => handleClick('feasibility')}
+        <div className="header__nav">
+          <Link
+            to="/feasibility"
+            className={
+              `header__nav-item ${isActived === "feasibility" ? "header__nav-item--active" : ""}`
+            }
+            onClick={() => handleClick("feasibility")}
           >
             특허등록가능성검토
           </Link>
-
-          <Link 
-            to='/patent-kr'
-            className={isActived === 'patentKr' ? 'active' : ''}
-            onClick={() => handleClick('patentKr')}
+  
+          <Link
+            to="/patent-kr"
+            className={
+              `header__nav-item ${isActived === "patentKr" ? "header__nav-item--active" : ""}`
+            }
+            onClick={() => handleClick("patentKr")}
           >
             국내특허등록신청
           </Link>
-
-          <Link 
-            to='/patent-gb'
-            className={isActived === 'patentGb' ? 'active' : ''}
-            onClick={() => handleClick('patentGb')}
+  
+          <Link
+            to="/patent-gb"
+            className={
+              `header__nav-item ${isActived === "patentGb" ? "header__nav-item--active" : ""}`
+            }
+            onClick={() => handleClick("patentGb")}
           >
             해외특허등록신청
           </Link>
-
-          <Link 
-            to='/costguide'
-            className={isActived === 'costguide' ? 'active' : ''}
-            onClick={() => handleClick('costguide')}
+  
+          <Link
+            to="/costguide"
+            className={
+              `header__nav-item ${isActived === "costguide" ? "header__nav-item--active" : ""}`
+            }
+            onClick={() => handleClick("costguide")}
           >
             비용안내
           </Link>
-
-          <Link 
-            to='/processguide'
-            className={isActived === 'processguide' ? 'active' : ''}
-            onClick={() => handleClick('processguide')}
+  
+          <Link
+            to="/processguide"
+            className={
+              `header__nav-item ${isActived === "processguide" ? "header__nav-item--active" : ""}`
+            }
+            onClick={() => handleClick("processguide")}
           >
             절차안내
           </Link>
@@ -91,6 +101,7 @@ const Header = () => {
       </div>
     </header>
   );
+  
 };
 
 export default Header;
